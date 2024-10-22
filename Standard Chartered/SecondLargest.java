e871fedbdded38ee25f9c3c0ec0cb898b1d0e67a
Shrimayee Adkar <166610308+shrimayee24@users.noreply.github.com>
2024-10-22 16:22:13 +0530

    Create SecondLargest.java

---
+++ Standard Chartered/SecondLargest.java
@@ -0,0 +1,39 @@
+//find 2nd largest element in array
+import java.util.* ;
+import java.io.*; 
+public class SecondLargest 
+{
+    public static int findSecondLargest(int n, int[] arr) 
+    {
+        if (n < 2) return -1;
+
+        // Initialize first and second largest
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+
+        for (int i = 0; i < n; i++) 
+        {
+			//compare number with largest and second largest
+//if >largest, assign it to largest
+//prev value of largest=secondlargest
+            // Update largest and secondLargest accordingly
+            if (arr[i] > largest) 
+            {
+                secondLargest = largest;
+                largest = arr[i];
+            } 
+			//if largest>number>secondlargest assign it to secondlargest
+            else if (arr[i] > secondLargest && arr[i] != largest) 
+            {
+                secondLargest = arr[i];
+            }
+        }
+		//if all elements are equal,only first if will execute in 1st iteration only.
+
+        // If secondLargest was not updated, return -1 (no distinct second largest element)
+        if (secondLargest == Integer.MIN_VALUE) return -1;
+
+        return secondLargest;
+    }
+}
